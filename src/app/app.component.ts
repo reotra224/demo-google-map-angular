1ce1732a7ab5d8b563f6864be9e28d68c30fa433
Ibrahima <ibrahima982011@koonda.fr>
2020-02-29 16:39:02 +0000

    Integrating Google Maps API w/ Angular 7+ written by Jocelyn Keung on MEDIUM.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'demo1-gmap';
+
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(14.7117003, -17.4684255),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+  }
 }
